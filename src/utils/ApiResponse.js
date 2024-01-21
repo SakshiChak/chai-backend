d7a90c4b3e5a49a2bf82d01aefcdae7f26852019
SakshiChak <mailme.sakshichak@gmail.com>
2024-01-21 22:59:27 +0530

    add comments

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,8 +1,24 @@
-class ApiResponse{
-    constructor(statusCode, data, message = "Success") {
-        this.statusCode = statusCode
-        this.data = data
-        this.message = message
-        this.success = statusCode < 400
+// Custom ApiResponse class for representing API responses
+class ApiResponse {
+    // Constructor for creating instances of ApiResponse
+    constructor(
+        statusCode,                 // HTTP status code of the API response
+        data,                       // Data payload of the API response
+        message = "Success"         // Default success message if not provided
+    ) {
+        // Sets the HTTP status code property
+        this.statusCode = statusCode;
+
+        // Sets the data payload property
+        this.data = data;
+
+        // Sets the message property
+        this.message = message;
+
+        // Sets the success property based on the HTTP status code
+        // If status code is less than 400, success is true; otherwise, it's false
+        this.success = statusCode < 400;
     }
 }
+
+export { ApiResponse }
