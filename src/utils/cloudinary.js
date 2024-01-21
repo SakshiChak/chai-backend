840dd99f1eb2e6cdc16c42dacde6aede84a3be38
SakshiChak <mailme.sakshichak@gmail.com>
2024-01-22 01:57:37 +0530

    .

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -20,8 +20,8 @@ const uploadOnCloudinary = async (localFilePath) => {
         });
 
         // file has been uploaded successfull
-        // console.log("file is uploaded on cloudinary", response.url);
-        fs.unlinkSync(localFilePath);
+        //console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
 
         // Return the Cloudinary response object
         return response;
