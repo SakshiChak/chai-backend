3ce473a27985bd796b219236f6ec553889b1f180
SakshiChak <mailme.sakshichak@gmail.com>
2024-01-21 23:07:22 +0530

    feat: Add MongoDB connection setup using Mongoose
    Establish an asynchronous connection to MongoDB using the Mongoose library. The connection details and success/failure messages are logged to the console. In case of a connection failure, the process exits with an error code.

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
+// Asynchronous function to establish a connection to MongoDB
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect
-            (`${process.env.MONGODB_URI}/${DB_NAME}`);
+        // Connecting to MongoDB using the Mongoose library
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        
+        // Logging successful connection details
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
+        // Handling errors during MongoDB connection
         console.log("MongoDB Connection Failed ", error);
-        process.exit(1);
+        process.exit(1); // Exiting the process with an error code if the connection fails
     }
 };
 
