e4902ec736bb5417fa472da11783c49c59989479
SakshiChak <mailme.sakshichak@gmail.com>
2024-01-21 23:05:20 +0530

    Configure server startup and database connection

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import {app} from './app.js'
 
+// Configuring environment variables from a .env file
 dotenv.config({
     path: "./env",
 });
 
+// Connecting to MongoDB
 connectDB()
 .then(() => {
+    // Handling errors during Express app initialization
     app.on("error", (error) => {
         console.log("ERROR: ", error);
         throw error;
-    })
+    });
+
+    // Starting the Express app and listening on the specified port or defaulting to 8000
     app.listen(process.env.PORT || 8000, () => {
         console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
     });
