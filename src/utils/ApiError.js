b67efb8aa1bdb7eb471bda214939f0811da5a20d
SakshiChak <mailme.sakshichak@gmail.com>
2024-01-21 22:57:58 +0530

    Add comments in ApiError.js file

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,23 +1,29 @@
-class ApiError extends Error{
+// Custom ApiError class extending the built-in Error class
+class ApiError extends Error {
+    // Constructor for creating instances of ApiError
     constructor(
-        statusCode,
-        message = "Something went wrong",
-        error = [],
-        stack = ""
+        statusCode,            // HTTP status code for the error
+        message = "Something went wrong",  // Default error message if not provided
+        errors = [],           // Array to store additional errors
+        stack = ""             // Stack trace for debugging purposes
     ) {
-        super(message)
-        this.statusCode = statusCode
-        this.data = null
-        this.message = message
-        this.success = false
-        this.errors = errors
-        
+        // Calls the constructor of the Error class with the provided message
+        super(message);
+
+        // Initializes properties specific to ApiError
+        this.statusCode = statusCode; // HTTP status code of the error
+        this.data = null;             // Placeholder for additional data (set to null)
+        this.message = message;       // Error message
+        this.success = false;         // Indicates whether the operation was successful (default is false)
+        this.errors = errors;         // Array to store additional errors
+
+        // Checks if a custom stack trace is provided, otherwise captures the stack trace
         if (stack) {
-            this.stack = stack           
+            this.stack = stack;       
         } else {
-            Error.captureStackTrace(this, this.constructor)
+            Error.captureStackTrace(this, this.constructor);
         }
     }
 }
 
-export {ApiError}
+export { ApiError }
