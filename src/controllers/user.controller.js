b459ec8f7dd6c4626abd75d09440d9f36f1e233b
SakshiChak <mailme.sakshichak@gmail.com>
2024-01-22 01:37:14 +0530

    feat: Handle coverImage file in the request
    Check if the "coverImage" file exists in the request and assign its local path to coverImageLocalPath variable.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -40,8 +40,9 @@ const registerUser = asyncHandler(async (req, res) => {
 
     // Get the local path of the avatar and cover image files
     const avatarLocalPath = req.files?.avatar[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    
     let coverImageLocalPath;
-
     // Check if coverImage file exists in the request
     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path
