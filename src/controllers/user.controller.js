93c378b9a8e7dfe79da13ab6439be12efc1bf353
SakshiChak <mailme.sakshichak@gmail.com>
2024-01-25 01:40:54 +0530

    Add user account management endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -211,7 +211,7 @@ const logoutUser = asyncHandler(async (req, res) => {
         .clearCookie("accessToken", options)
         .clearCookie("refreshToken", options)
         .json(new ApiResponse(200, {}, "User logged out"));
-});
+})
 
 // Define an asynchronous route handler for refreshing access token
 const refreshAccessToken = asyncHandler(async (req, res) => {
@@ -270,10 +270,196 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
         // Throw an error if any issues occur during the token refresh process
         throw new ApiError(401, error?.message || "Invalid refresh token");
     }
-});
+})
+
+const changeCurrentPassword = asyncHandler(async(req, res) => {
+    // Destructuring oldPassword and newPassword from the request body
+    const { oldPassword, newPassword } = req.body;
+
+    // Finding the user by ID using the User model
+    const user = await User.findById(req.user?._id);
+
+    // Checking if the provided old password is correct using the user's method
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+    // If the old password is incorrect, throw an ApiError with status code 400
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Invalid old password");
+    }
+
+    // Setting the user's password to the new password
+    user.password = newPassword;
+
+    // Saving the user's document with validateBeforeSave set to false
+    // This is done to bypass Mongoose validation as it may require the old password
+    await user.save({ validateBeforeSave: false });
+
+    // Returning a JSON response with status code 200 and a success message
+    return res
+        .status(200)
+        .json(new ApiResponse(200, {}, "Password changed successfully"));
+})
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+    // Returning a JSON response with status code 200 and the current user information
+    return res
+    .status(200)
+    .json(new ApiResponse(
+        200,
+        req.user, // The user object obtained from the authenticated request
+        "User fetched successfully"
+    ))
+})
+
+const updateAccountDetails = asyncHandler(async(req, res) => {
+    // Destructuring fullName and email from the request body
+    const { fullName, email } = req.body;
+
+    // Checking if fullName and email are provided in the request body
+    if (!fullName || !email) {
+        // Throwing an ApiError with status code 400 and a message if any field is missing
+        throw new ApiError(400, "All fields are required");
+    }
+
+    // Updating the user's account details using the User model and findByIdAndUpdate
+    const user = await User.findByIdAndUpdate(
+        req.user?._id, // User ID obtained from the authenticated request
+        {
+            $set: {
+                fullName, // Updating fullName field
+                email: email, // Updating email field
+            },
+        },
+        { new: true } // Returning the updated document instead of the original
+    ).select("-password"); // Excluding the password field from the response
+
+    // Returning a JSON response with status code 200 and a success message
+    return res
+        .status(200)
+        .json(new ApiResponse(200, user, "Account details updated successfully"));
+})
+
+const updateUserAvatar = asyncHandler(async (req, res) => {
+    // Getting the local path of the uploaded avatar file from the request
+    const avatarLocalPath = req.file?.path;
+
+    // If the avatar file is missing, throw an ApiError with status code 400
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is missing");
+    }
+
+
+    // TODO: Delete old image - Assignment (You can add the logic for deleting the old image here)
+
+    // Fetching the user to get the current avatar URL
+    const currentUser = await User.findById(req.user?._id);
+
+    // Deleting the old avatar from Cloudinary if it exists
+    if (currentUser.avatar) {
+        // Extracting the public ID from the Cloudinary URL
+        const publicId = currentUser.avatar.split('/').pop().split('.')[0];
+
+        try {
+            // Deleting the old avatar using the Cloudinary API
+            await cloudinary.uploader.destroy(publicId);
+        } catch (error) {
+            // Handling errors that may occur during the deletion process
+            throw new ApiError(500, 'Internal Server Error');
+        }
+    }
+
+
+    // Uploading the avatar image to Cloudinary and obtaining the result
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+    // If there is an error while uploading the avatar, throw an ApiError with status code 400
+    if (!avatar.url) {
+        throw new ApiError(400, "Error while uploading the avatar");
+    }
+
+    // Updating the user's avatar URL in the database using findByIdAndUpdate
+    const user = await User.findByIdAndUpdate(
+        req.user?._id, // User ID obtained from the authenticated request
+        {
+            $set: {
+                avatar: avatar.url, // Updating the avatar field with the new URL
+            },
+        },
+        { new: true } // Returning the updated document instead of the original
+    ).select("-password"); // Excluding the password field from the response
+
+    // Returning a JSON response with status code 200 and a success message
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, user, "Avatar image updated successfully")
+        );
+})
+
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+    // Getting the local path of the uploaded cover image file from the request
+    const coverImageLocalPath = req.file?.path;
+
+    // If the cover image file is missing, throw an ApiError with status code 400
+    if (!coverImageLocalPath) {
+        throw new ApiError(400, "Cover image file is missing");
+    }
+
+    // TODO: Delete old image - Assignment (You can add the logic for deleting the old cover image here)
+
+    // Fetching the user to get the current cover image URL
+    const currentUser = await User.findById(req.user?._id);
+
+    // Deleting the old cover image from Cloudinary if it exists
+    if (currentUser.coverImage) {
+        // Extracting the public ID from the Cloudinary URL
+        const publicId = currentUser.coverImage.split('/').pop().split('.')[0];
+
+        try {
+            // Deleting the old cover image using the Cloudinary API
+            await cloudinary.uploader.destroy(publicId);
+        } catch (error) {
+            // Handling errors that may occur during the deletion process
+            throw new ApiError(500, 'Internal Server Error');
+        }
+    }
+
+    // Uploading the cover image to Cloudinary and obtaining the result
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    // If there is an error while uploading the cover image, throw an ApiError with status code 400
+    if (!coverImage.url) {
+        throw new ApiError(400, "Error while uploading the cover image");
+    }
+
+    // Updating the user's cover image URL in the database using findByIdAndUpdate
+    const user = await User.findByIdAndUpdate(
+        req.user?._id, // User ID obtained from the authenticated request
+        {
+            $set: {
+                coverImage: coverImage.url, // Updating the coverImage field with the new URL
+            },
+        },
+        { new: true } // Returning the updated document instead of the original
+    ).select("-password"); // Excluding the password field from the response
+
+    // Returning a JSON response with status code 200 and a success message
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(200, user, "Cover image updated successfully")
+        );
+})
+
 
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken};
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
+};
